594e45f67a15992154221247979966e2cb441a1c
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-09-10 13:17:47 +0200

    integrated bmpn-js, added basic loading/saving of BPMN file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,8 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -11,6 +13,14 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,101 @@
 <script lang="ts">
-  import Greet from './lib/Greet.svelte'
+  import "bpmn-js/dist/assets/bpmn-font/css/bpmn.css";
+  import "bpmn-js/dist/assets/diagram-js.css";
+  import BpmnModeler from "bpmn-js/lib/Modeler";
+  import { onDestroy, onMount } from "svelte";
+  import { open, save } from "@tauri-apps/api/dialog";
+  import { readTextFile, writeTextFile } from "@tauri-apps/api/fs";
+
+  let container;
+  let modeler;
+
+  onMount(async () => {
+    modeler = new BpmnModeler({
+      container: container,
+      // keyboard: { bindInfo: document }
+    });
+    modeler.createDiagram();
+  });
+
+  onDestroy(() => {
+    modeler.destroy();
+  });
+
+  async function saveBPMN() {
+    try {
+      const { xml } = await modeler.saveXML({ format: true });
+      const filePath = await save({
+        filters: [{ name: "BPMN", extensions: ["bmpn"] }],
+      });
+      if (filePath !== null) {
+        await writeTextFile(filePath, xml);
+        // FIXME success dialog
+      }
+    } catch (err) {
+      // FIXME error handling
+      console.error(err);
+    }
+  }
+
+  async function savePNG() {
+    try {
+      const { svg } = await modeler.saveSVG();
+      // FIXME save to file
+      // FIXME handle PNG conversion
+    } catch (err) {
+      // FIXME error handling
+      console.error(err);
+    }
+  }
+
+  async function loadBPMN() {
+    try {
+      const filePath = await open({
+        multiple: false,
+        filters: [
+          {
+            name: "WHatIsIt",
+            extensions: ["bpmn", "xml"],
+          },
+        ],
+      });
+      if (filePath !== null) {
+        const fileContent = await readTextFile(filePath as string);
+        await modeler.importXML(fileContent);
+      }
+    } catch (err) {
+      // FIXME error handling
+      console.log(err);
+    }
+  }
 </script>
 
-<main class="container">
-  <h1>Welcome to Tauri!</h1>
-
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-    </a>
+<main class="main">
+  <div class="toolbar">
+    <button on:click|stopPropagation|preventDefault={savePNG}>PNG</button>
+    <button on:click|stopPropagation|preventDefault={saveBPMN}>Save</button>
+    <button on:click|stopPropagation|preventDefault={loadBPMN}>Load</button>
   </div>
-
-  <p>
-    Click on the Tauri, Vite, and Svelte logos to learn more.
-  </p>
-
-  <div class="row">
-    <Greet />
-  </div>
-
-
+  <div class="modeler" bind:this={container} />
 </main>
 
 <style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
+  .toolbar {
+    background-color: aquamarine;
+    display: flex;
+    flex-direction: row-reverse;
+    position: absolute;
+    right: 5px;
+    top: 5px;
+    z-index: 1000;
   }
-
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
+  .toolbar button {
+    height: 25px;
+    margin: 0 2px 0 0;
+    width: 50px;
+  }
+  .modeler {
+    height: 100vh;
+    min-height: 100vh;
   }
 </style>
