2fc5f994802eece4e8cafd8012c2edaa8f2deda1
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-09-10 16:46:55 +0200

    feat: first working version #1
    * initial tauri setup
    * integrated bmpn-js, added basic loading/saving of BPMN file
    * saving to PNG works
    * show loaded file name as app title
    * added error display

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,20 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use tauri::Manager;
+
+fn main() {
+    tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/App.svelte
@@ -0,0 +1,162 @@
+<script lang="ts">
+  import { open, save } from "@tauri-apps/api/dialog";
+  import {
+    readTextFile,
+    writeBinaryFile,
+    writeTextFile,
+  } from "@tauri-apps/api/fs";
+  import { appWindow } from "@tauri-apps/api/window";
+  import "bpmn-js/dist/assets/bpmn-font/css/bpmn.css";
+  import "bpmn-js/dist/assets/diagram-js.css";
+  import BpmnModeler from "bpmn-js/lib/Modeler";
+  import { Canvg, presets } from "canvg";
+  import { onDestroy, onMount } from "svelte";
+  import Error from "./lib/Error.svelte";
+  import { errorMessage } from "./store";
+
+  const appTitle = "BPMN Modeler";
+  let container;
+  let modeler;
+  let canvas;
+
+  onMount(async () => {
+    modeler = new BpmnModeler({
+      container: container,
+      // FIXME check keyboard bindings
+      // keyboard: { bindInfo: document }
+    });
+    modeler.createDiagram();
+  });
+
+  onDestroy(() => {
+    modeler.destroy();
+  });
+
+  let loadedFilePath = "";
+
+  async function setLoadedFilePath(filePath: string) {
+    loadedFilePath = filePath;
+    let title = appTitle;
+    if (filePath.length > 0) {
+      await appWindow.setTitle(`${appTitle} - ${filePath}`);
+    } else {
+      await appWindow.setTitle(title);
+    }
+  }
+
+  async function saveBPMNFile(filePath: string) {
+    try {
+      const { xml } = await modeler.saveXML({ format: true });
+      await writeTextFile(filePath, xml);
+    } catch (err) {
+      console.log(err);
+      errorMessage.update((m) => err);
+    }
+  }
+
+  async function saveBPMN() {
+    if (loadedFilePath.length > 0) {
+      saveBPMNFile(loadedFilePath);
+    } else {
+      saveAsBPMN();
+    }
+  }
+
+  async function saveAsBPMN() {
+    try {
+      const filePath = await save({
+        filters: [{ name: "BPMN", extensions: ["bpmn"] }],
+      });
+      if (filePath !== null) {
+        saveBPMNFile(filePath);
+        await setLoadedFilePath(filePath);
+      }
+    } catch (err) {
+      console.log(err);
+      errorMessage.update((m) => err);
+    }
+  }
+
+  async function savePNG() {
+    try {
+      const filePath = await save({
+        filters: [{ name: "PNG", extensions: ["png"] }],
+      });
+      if (filePath !== null) {
+        const { svg } = await modeler.saveSVG();
+
+        const ctx = canvas.getContext("2d");
+        const v = await Canvg.from(ctx, svg, presets.offscreen());
+        await v.render();
+
+        canvas.toBlob(async (blob) => {
+          const content = await blob.arrayBuffer();
+          await writeBinaryFile(filePath, content);
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      errorMessage.update((m) => err);
+    }
+  }
+
+  async function loadBPMN() {
+    try {
+      const filePath = await open({
+        multiple: false,
+        filters: [
+          {
+            name: "Model",
+            extensions: ["bpmn", "xml"],
+          },
+        ],
+      });
+      if (filePath !== null) {
+        const fileContent = await readTextFile(filePath as string);
+        await modeler.importXML(fileContent);
+        await setLoadedFilePath(filePath as string);
+      }
+    } catch (err) {
+      console.log(err);
+      errorMessage.update((m) => err);
+    }
+  }
+
+  async function newDiagram() {
+    modeler.createDiagram();
+    await setLoadedFilePath("");
+  }
+</script>
+
+<main class="main">
+  <div class="toolbar">
+    <button on:click|stopPropagation|preventDefault={loadBPMN}>Load</button>
+    <button on:click|stopPropagation|preventDefault={saveBPMN}>Save</button>
+    <button on:click|stopPropagation|preventDefault={saveAsBPMN}>SaveAs</button>
+    <button on:click|stopPropagation|preventDefault={savePNG}>PNG</button>
+    <button on:click|stopPropagation|preventDefault={newDiagram}>New</button>
+  </div>
+  <Error />
+  <div class="modeler" bind:this={container} />
+</main>
+<canvas bind:this={canvas} />
+
+<style>
+  .toolbar {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    right: 2px;
+    top: 2px;
+    z-index: 1000;
+  }
+  .toolbar button {
+    height: 25px;
+    margin: 0 2px 0 0;
+    width: 60px;
+  }
+  .modeler {
+    height: 100vh;
+    min-height: 100vh;
+  }
+</style>
