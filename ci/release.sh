7a6617496eb999621c1536c5fdd62055dd2c80ab
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-10-04 21:21:20 +0200

    fix: fixed release scripts #15

--- ci/release.sh
+++ ci/release.sh
@@ -3,7 +3,7 @@ VERSION=$1
 DATE=`date -u '+%Y-%m-%dT%H:%M:%SZ'`
 
 sed -i 's/^version = ".*"$/version = "'"${VERSION}"'"/g' src-tauri/Cargo.toml
-sed -i '/name = "remembrancer"/{ n; s/version = ".*"/version = "'"${VERSION}"'"/g }' src-tauri/Cargo.lock
+sed -i '/name = "bpmn-modeler"/{ n; s/version = ".*"/version = "'"${VERSION}"'"/g }' src-tauri/Cargo.lock
 sed -i 's/^    "version": ".*"$/    "version": "'"${VERSION}"'"/g' src-tauri/tauri.conf.json
 sed -i 's/^  "version": ".*",$/  "version": "'"${VERSION}"'",/g' package.json
 sed -i 's/^  "version": ".*",$/  "version": "'"${VERSION}"'",/g' package-lock.json
