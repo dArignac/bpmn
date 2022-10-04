392f4765dcaa5e024ed37006617cafa31fca7a40
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-10-04 21:20:44 +0200

    fixed update script #15

--- create-update.sh
+++ create-update.sh
@@ -5,9 +5,9 @@ SIGNATURE_MACOS_FILE_DIR=$3
 SIGNATURE_WINDOWS_FILE_DIR=$4
 
 # restored from github artifacts
-SIGNATURE_LINUX=`cat ${SIGNATURE_LINUX_FILE_DIR}/bpmn_*_amd64.AppImage.tar.gz.sig`
+SIGNATURE_LINUX=`cat ${SIGNATURE_LINUX_FILE_DIR}/bpmn-modeler_*_amd64.AppImage.tar.gz.sig`
 SIGNATURE_MACOS=`cat ${SIGNATURE_MACOS_FILE_DIR}/*.app.tar.gz.sig`
-SIGNATURE_WINDOWS=`cat ${SIGNATURE_WINDOWS_FILE_DIR}/bpmn_*_x64_en-US.msi.zip.sig`
+SIGNATURE_WINDOWS=`cat ${SIGNATURE_WINDOWS_FILE_DIR}/bpmn-modeler_*_x64_en-US.msi.zip.sig`
 DATE=`date -u '+%Y-%m-%dT%H:%M:%SZ'`
 
 tee update.json <<EOF
@@ -18,15 +18,15 @@ tee update.json <<EOF
   "platforms": {
     "darwin-x86_64": {
       "signature": "${SIGNATURE_MACOS}",
-      "url": "https://github.com/dArignac/bpmn/releases/download/v${VERSION}/bpmn.app.tar.gz"
+      "url": "https://github.com/dArignac/bpmn/releases/download/v${VERSION}/bpmn-modeler.app.tar.gz"
     },
     "linux-x86_64": {
       "signature": "${SIGNATURE_LINUX}",
-      "url": "https://github.com/dArignac/bpmn/releases/download/v${VERSION}/bpmn_${VERSION}_amd64.AppImage.tar.gz"
+      "url": "https://github.com/dArignac/bpmn/releases/download/v${VERSION}/bpmn-modeler_${VERSION}_amd64.AppImage.tar.gz"
     },
     "windows-x86_64": {
       "signature": "${SIGNATURE_WINDOWS}",
-      "url": "https://github.com/dArignac/bpmn/releases/download/v${VERSION}/bpmn_${VERSION}_x64_en-US.msi.zip"
+      "url": "https://github.com/dArignac/bpmn/releases/download/v${VERSION}/bpmn-modeler_${VERSION}_x64_en-US.msi.zip"
     }
   }
 }
