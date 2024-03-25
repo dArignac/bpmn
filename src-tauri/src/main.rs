621a2f310151a74120f54b7c1cd069ad2d71be5f
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2024-03-25 20:34:18 +0100

    fix: adds debug for app arguments

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{env, ffi::OsString};
 use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
+            // FIXME remove once testing is done
+            let args: Vec<String> = env::args().collect();
+            let args_os: Vec<OsString> = env::args_os().collect();
+            println!("args: {:?}", args);
+            println!("argsOs: {:?}", args_os);
             #[cfg(debug_assertions)]
             {
                 let window = app.get_window("main").unwrap();
